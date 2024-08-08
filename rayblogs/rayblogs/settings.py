080739e5dcf52e306001021b06ff652ccf1835e4
rayhan <156523736+TheRayhan009@users.noreply.github.com>
2024-08-08 23:04:16 +0600

    Add files via upload

--- rayblogs/rayblogs/settings.py
+++ rayblogs/rayblogs/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "blogs",
+    "signin",
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_ROOT=BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- rayblogs/rayblogs/views.py
+++ rayblogs/rayblogs/views.py
@@ -1,6 +1,86 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse ,HttpResponseRedirect
+from blogs.models import Blogs
+from django.core.paginator import Paginator
+from signin.models import Users
+
 
 
 def home(request):
-    return render(request,"home.html")
+    
+    ele={
+        "element": Blogs.objects.all().order_by("date_of_post").reverse()
+    }
+    
+    return render(request,"home.html",ele)
+
+def signin(request):
+    
+    if request.method=="POST":
+        fname=request.POST.get("fristname")
+        lname=request.POST.get("lastname")
+        email=request.POST.get("email")
+        phone_num=request.POST.get("phone")
+        Username=request.POST.get("Uname")
+        password=request.POST.get("pass")
+
+        Updata=Users(first_name=fname,last_name=lname,email=email,phone_number=phone_num,user_name=Username,password=password)
+        Updata.save()
+        
+        return HttpResponseRedirect("/")
+        
+    return render(request,"signin.html")
+
+
+
+def login(request):
+    
+    if request.method=="POST":
+        Username=request.POST.get("Uname")
+        password=request.POST.get("pass")
+
+        Sdata=Users.objects.filter(user_name=Username,password=password)
+        
+        if Sdata:
+            print("You LogedIn!!")
+        else:
+            print("Who are you???")
+        
+        return HttpResponseRedirect("/")
+    return render(request,"login.html")
+
+
+def Dblog(request,link):
+    ele={
+        "element":Blogs.objects.get(A_slug=link)
+    }
+    return render(request,"Dblog.html",ele)
+
+
+def search(request):
+    s=""
+    data=Blogs.objects.all()
+    if request.method=="GET":
+        s=request.GET.get("usearch")
+        data=Blogs.objects.filter(titel__icontains=s)
+        ele={
+            "data":data,
+        }
+    
+    return render(request,"search.html",ele)
+
+def blog(request):
+    data=Blogs.objects.all()
+    page=Paginator(data,1)
+    x=request.GET.get("page")
+    filan=page.get_page(x)
+    
+    co=page.num_pages
+    
+    ele={
+        "element":filan,
+        "r":range(1,co+1)
+    }
+    return render(request,"allblogs.html",ele)
+
+
