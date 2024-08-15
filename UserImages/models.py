b500b1f7ffd223cbe8548376e36ddd45f6f55e61
rayhan <156523736+TheRayhan009@users.noreply.github.com>
2024-08-15 10:08:50 +0600

    Add files via upload

--- UserImages/models.py
+++ UserImages/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-
+from autoslug import AutoSlugField
 # Create your models here.
 
 class UPImage(models.Model):
     UPimage = models.ImageField(upload_to='images')
     UPtitel=models.CharField(max_length=500)
     Pdate=models.DateField(auto_now_add=True)
+    Plikes=models.IntegerField(default=0,null=True)
+    Pdislikes=models.IntegerField(default=0,null=True)
     PUimage=models.ImageField()
     PUname=models.CharField(max_length=30)
+    
+    A_imageSlug=AutoSlugField(populate_from='UPtitel',unique=True,null=True,default=None)
